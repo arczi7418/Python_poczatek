7f6145393e401f54fb5797bc7ad5566cdb785e7f
Artur Jankowski <Arti08@onet.pl>
2026-05-09 16:15:30 +0200

    Zadanie z klasami i obiektami

---
+++ Rozne sposoby importu/shop/class and objects.py
@@ -0,0 +1,17 @@
+class Products:
+    pass
+
+class Order:
+    pass
+
+class Apple:
+    pass
+
+class Potato:
+    pass
+
+owoc = Apple()
+warzywo = Potato()
+
+print(type(owoc))
+print(type(warzywo))
